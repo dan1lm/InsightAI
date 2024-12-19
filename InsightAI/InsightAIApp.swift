48f96c0c5b8a5599f01045e9a348c128710a98f1
Danil Merinov <danilmerinov9@gmail.com>
2024-12-18 21:07:06 -0500

    Initial Commit

---
+++ InsightAI/InsightAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightAIApp.swift
+//  InsightAI
+//
+//  Created by Danil Merinov on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct InsightAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
