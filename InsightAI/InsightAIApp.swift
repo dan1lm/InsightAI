45bf16dafb409e6f3ababa78af8f245eba78338c
Danil Merinov <danilmerinov9@gmail.com>
2025-01-09 13:50:07 -0500

    added app delegate

--- InsightAI/InsightAIApp.swift
+++ InsightAI/InsightAIApp.swift
@@ -5,16 +5,20 @@
 //  Created by Danil Merinov on 12/18/24.
 //
 
+import Spezi
 import SwiftUI
+import HealthKit
 
 @main
 struct InsightAIApp: App {
+    @ApplicationDelegateAdaptor(SpeziAppDelegate.self) var appDelegate
     @StateObject var manager = HealthManager()
     
     var body: some Scene {
         WindowGroup {
             InsightAITabView()
                 .environmentObject(manager)
+                .spezi(appDelegate)
         }
     }
 }
